dec880d71acd921c427a56d178773a3194a7ceac
Madeline Grimes <mkg420@stern.nyu.edu>
2021-05-25 20:42:52 -0400

    Update game.py

--- game.py
+++ game.py
@@ -2,3 +2,16 @@ print("Rock, Paper, Scissors, Shoot!")
 
 user_choice = input("Please choose one of 'rock', 'paper', 'scissors': ")
 print(user_choice)
+
+
+
+if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors"):
+    print("VALID. KEEP GOING")
+else:
+    print("OOPS, INVALID INPUT. PLEASE TRY AGAIN.")
+    quit()
+
+
+print("This is the end of our game! Please play again.")
+
+# validate the input
